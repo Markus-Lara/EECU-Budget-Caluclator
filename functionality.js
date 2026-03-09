1995759d08475cdad191e5cbc39ba6c6b0d2cdb7
Johnny-A09 <alvarado.johnny159@gmail.com>
2026-03-09 10:40:01 -0700

    everything
    mainly fixes in HTML and css

--- functionality.js
+++ functionality.js
@@ -1,28 +1,3 @@
-function init() {
-    const steps = document.querySelectorAll(".step a"); // Select all step circles
-    const content = document.querySelectorAll(".step-content"); // Select all content sections
-    steps.forEach((step, index) => {
-        step.addEventListener("click", () => {
-            // Remove active class from all steps
-            document.querySelectorAll(".step").forEach(step => step.classList.remove("active"));
-            // Add active class to clicked step
-            step.parentElement.classList.add("active");
-
-            // add logic to hide / reveal elements based on the step
-            content.forEach((section, i) => {
-                if (i === index) {
-                    section.classList.add("active");
-                } else {
-                    section.classList.remove("active");
-                }
-            });
-
-        });
-    });
-}
-
-
-
 document.getElementById("career").addEventListener("change", function() {
     const num = this.value;
     const job = document.getElementById("income-amount");
@@ -32,7 +7,9 @@ document.getElementById("career").addEventListener("change", function() {
 });
 
 
-
+function estimated(){//adds value into estimated table
+    document.getElementById('student-est').innerHTML = `${}`//call function that produces value for this section here when it's created
+}
 
 
 
