75b974d47a0a4e480c3ae513dc4443d5c1000b7e
Cr-06 <colemrose@gmail.com>
2026-03-10 09:47:06 -0700

    index page jobs updates and start of page 5
    worked mainly on JS for the index page and started on the HTML of page 5, I just copied and pasted the table code so tags need to be fixed

--- functionality.js
+++ functionality.js
@@ -1,16 +1,25 @@
-document.getElementById("career").addEventListener("change", function() {
-    const num = this.value;
-    const job = document.getElementById("income-amount");
-    job.textContent = num;
-    console.log(job);
-    return;
+const careerSelect = document.getElementById("career");
+const incomeDisplay = document.getElementById("income-amount");
+
+fetch("careers.json") // Pulls data from the JSON, which has all occupations
+    .then(response => response.json())
+    .then(data => {
+
+        data.forEach(job => {
+            const option = document.createElement("option");
+
+            option.value = job.Salary;
+            option.textContent = job.Occupation;
+
+            careerSelect.appendChild(option);
+        });
+
+    });
+
+careerSelect.addEventListener("change", function () { // for the salaries
+
+    const salary = Number(this.value);
+
+    incomeDisplay.textContent = "$ " + salary.toLocaleString();
+
 });
-
-
-function estimated(){//adds value into estimated table
-    document.getElementById('student-est').innerHTML = `${}`//call function that produces value for this section here when it's created
-}
-
-
-
-document.addEventListener("DOMContentLoaded", init);
